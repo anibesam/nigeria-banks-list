68ebe78151daaf28f7e2bbb8c50068bcf133d873
anibesam <samanibe@yahoo.com>
2024-12-20 22:33:47 +0100

    fix pkg

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../lib/index");
+const index_1 = require("../index");
 // Test getting all banks
 console.log((0, index_1.getBanks)());
 // Test finding a specific bank by code
